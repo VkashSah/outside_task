d42d4bcfdc992836950a76b1a6462d7ed1605cb8
Vikash Sah <mevkashsah@gmail.com>
2021-12-20 10:10:26 +0545

    product GET for home page

---
+++ modules/product/product.js
@@ -0,0 +1,56 @@
+let product = {
+  coke: {
+    _id: 1,
+    productName: "coke",
+    price: 20,
+    quantity: 10,
+  },
+  pepsi: {
+    _id: 2,
+    productName: "pepsi",
+    price: 25,
+    quantity: 10,
+  },
+  dew: {
+    _id: 3,
+    productName: "dew",
+    price: 30,
+    quantity: 10,
+  },
+};
+
+let products = {};
+
+products.checkTender = (productPrice, tender, quantity) => {
+  if (productPrice * quantity > tender) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+products.checkStock = (productQuantity, quantity) => {
+  if (productQuantity >= quantity) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+products.productQuantityDecrement = (
+  productName,
+  availableQuantity,
+  quantity
+) => {
+  product[productName].quantity = availableQuantity - quantity;
+};
+
+products.productQuantityIncrement = (
+  productName,
+  availableQuantity,
+  quantity
+) => {
+  product[productName].quantity = availableQuantity + quantity;
+};
+
+module.exports = { product, products };
--- modules/product/productController.js
+++ modules/product/productController.js
@@ -1,26 +1,14 @@
+const product = require("./product");
 const productController = {};
-
-let product = [
-  {
-    productName: "coke",
-    price: 20,
-    quantity: 10,
-  },
-  {
-    productName: "pepsi",
-    price: 25,
-    quantity: 10,
-  },
-  {
-    productName: "dew",
-    price: 30,
-    quantity: 10,
-  },
-];
 
 productController.getProduct = (req, res) => {
   try {
-    res.status(200).json({ product, message: "Product get success!!!" });
+    res
+      .status(200)
+      .json({
+        stockAvailable: product.product,
+        message: "Product get success!!!",
+      });
   } catch (err) {
     res.status(500).json({ err, error: "Internal server error" });
   }
